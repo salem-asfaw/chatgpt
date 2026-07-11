2f3d771db39c720527573fd2413568cb88acdac4
salem asfaw <salemasfaw7@gmail.com>
2026-07-11 13:00:46 +0300

    add google genai dependency

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import db from "./db/db.config.js";
 import express from "express";
 import cors from "cors";
 import mainRouter from "./src/api/main.routes.js";
-import { ErrorHandler } from "./src/middleware/error.handler.js";
+import { ErrorHandler } from "./src/middleware/error-hander.js";
 
 const app = express();
 
